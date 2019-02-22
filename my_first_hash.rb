1ebcd5794a5ce0a4e0ae8f3c03ca39350bf71171
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-02-22 15:22:25 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,5 @@
 def my_hash
-  your_hash = {"name" => "Ranjit", "primary occupation" => ""}
+  your_hash = {"name" => "Ranjit", "primary occupation" => "author", "secondary occupation" => "entrapeneur"}
   
 end
 
